21329d6107445ee43b6b8a73ec852cde5a71b16d
Aaron Crawfis <aacrawfi@microsoft.com>
2020-01-08 08:30:51 -0800

    HelloWorld

---
+++ QSharpHelloWorld/Driver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using Microsoft.Quantum.Intrinsic;
+
+namespace QSharpHelloWorld
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            int nbExperiments = 10000;
+            int oneCount = 0;
+            using (var qsim = new QuantumSimulator())
+            {
+                for(int i=0; i<nbExperiments; i++)
+                {
+                    var r = HelloQ.Run(qsim).Result;
+                    if (r.ToString() == "One")
+                    {
+                        Console.Write("X");
+                        oneCount++;
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine(string.Format("\n\nProbability of 1s: {0:P2}", oneCount * 1.0 / nbExperiments));
+            }
+        }
+    }
+}
