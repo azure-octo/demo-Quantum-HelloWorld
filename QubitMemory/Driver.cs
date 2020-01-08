aafab84384040bb1e713f7c4b8707d0608110cf8
Aaron Crawfis <aacrawfi@microsoft.com>
2020-01-08 08:44:36 -0800

    Add qubit memory

---
+++ QubitMemory/Driver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace QubitMemory
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                int count = 35;
+                double[] m = new double[count];
+                for (int i=0; i<count; i++)
+                {
+                    qsim.QubitManager.Allocate(1);
+                    m[i] = Process.GetCurrentProcess().WorkingSet64;
+                    Console.WriteLine(string.Format("# of qubits: {0:00}:\t\tMemory usage: {1:0.###}G", (i+1), m[i]/(1024*1024*1024)));
+                }
+            }
+        }
+    }
+}
